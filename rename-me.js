2c566d4aedbb9642ea0603da8c1b75b56cbe6e14
cobyrpilpel <cobyrpilpel@Cobys-MacBook-Pro.local>
2014-07-18 15:39:48 -0400

    SDI Project 2
    attempt 3 project 2

---
+++ rename-me.js
@@ -0,0 +1,22 @@
+//alert("JavaScript works!");
+
+//Coby Pilpel
+//july 17th 2014
+//SDI Project 2
+
+
+
+
+
+var forScience = "We can do it! FOR SCIENCE!"
+var survivalChance = 45
+var astronautSafety = false
+
+
+
+//Code
+//Start
+Console.log("Welcome to MASA! Here at the Martian Air and Space Association there is no failure! Only examples of unintentional scientific explosions! 
+//Confirm
+
+var astronautSafety = prompt("For our current mission the chance of the astronaut surviving is" + survivalChance + ". We think we could achieve more sciencish things if we take more risks. Do you agree?"
