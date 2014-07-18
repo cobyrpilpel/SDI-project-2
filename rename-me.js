dc960c7a9d0fd9a745deb65625f7acf42cc16eec
cobyrpilpel <cobyrpilpel@Cobys-MacBook-Pro.local>
2014-07-18 17:52:40 -0400

    I think I did that right
    Boolean complete

--- rename-me.js
+++ rename-me.js
@@ -8,10 +8,10 @@
 
 
 var rocketShip = "MSS. Steve";
-var numberOfSurvivors;
+var survivingCrewNumber;
 var targetOne = "Pluto";
 var targetTwo = "Venus"
-var numberOfTestDummies = 2;
+var numberOfDummies = 10;
 var primaryTarget = "Pluto";
 var secondaryTarget = "Venus";
 
@@ -23,4 +23,42 @@ console.log("Welcome to MASA! Here at the Martian Air and Space Association ther
 
 var targetSelected = prompt("What planet will the " + rocketShip + " crash....... I MEAN land on? ", targetOne + " or " + targetTwo);
 
-//
+//PROCEDURE
+
+var targetPlanet = function (targetSelected, targetOne, targetTwo) {
+
+var destination = targetPlanet
+    if (targetSelected === targetOne || targetSelected === targetTwo) 
+    {console.log("The target you have chosen for" + rocketShip +" is a " + destination + ".");
+    } else {console.log("That isn't a good target. Who in their right mind would want to go there?"); };
+ 
+ targetPlanet(targetSelected, targetOne, targetTwo)   
+
+//CONFIRM
+
+var testDummies = confirm("We must have some test dummies up to the challenge right?")  
+console.log("Let's test them just to be sure.")
+
+//BOOLEAN FUNCTION
+
+var testDummiesAvailable = function (testDummies){
+var yayForDummies = testDummies
+var numberOfDummies = 10
+    while (testDummies === true && numberOfDummies > 2)
+    {numberOfDummies--;
+    	console.log("While it is " + testDummies + " that we have available dummies we need to be sure they are up to the task.");
+    	console.log("Let the tests commence!");
+    	console.log("Was he supposed to explode like that?");
+    	console.log("Well that one didn't make the cut. We have " + numberOfDummies + " remaining.");}
+console.log("We have found our dummies! These dummies have passed the tests and will fly this mission.")
+return yayForDummies ;}
+ 
+ 
+//NUMBER FUNCTION
+
+    
+    
+       
+        
+        
+
